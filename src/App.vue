e1cca03e4c27deded3deada7ef5ea4fbd9260755
Nils Rolfsmeier <n.rolfsmeier@audatis.de>
2025-02-18 09:50:14 +0100

    init

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,9 @@
 <template>
   <v-app>
     <v-main>
-      <router-view></router-view>
+      <router-view />
     </v-main>
   </v-app>
 </template>
 
-<style scoped>
-.v-card-title {
-  font-weight: 600;
-}
-</style>
+<style scoped></style>
